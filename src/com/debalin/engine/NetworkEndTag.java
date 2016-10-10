15afec3e91ed1250d5e388d6802040667639af5a
Debalin Das <debalin90@gmail.com>
2016-10-10 00:25:58 -0400

    Trying some optimizations.

--- src/com/debalin/engine/NetworkEndTag.java
+++ src/com/debalin/engine/NetworkEndTag.java
@@ -1,6 +1,14 @@
 package com.debalin.engine;
 
+import processing.core.PVector;
+
 public class NetworkEndTag extends GameObject {
+
+  protected transient PVector color;
+  protected transient PVector position;
+  protected transient PVector velocity;
+  protected transient PVector acceleration;
+  protected transient PVector size;
 
   public NetworkEndTag() {
     tag = GameServer.NetworkTag.END_TAG;
--- src/com/debalin/engine/NetworkStartTag.java
+++ src/com/debalin/engine/NetworkStartTag.java
@@ -1,6 +1,14 @@
 package com.debalin.engine;
 
+import processing.core.PVector;
+
 public class NetworkStartTag extends GameObject {
+
+  protected transient PVector color;
+  protected transient PVector position;
+  protected transient PVector velocity;
+  protected transient PVector acceleration;
+  protected transient PVector size;
 
   public NetworkStartTag(int connectionID) {
     tag = GameServer.NetworkTag.START_TAG;
