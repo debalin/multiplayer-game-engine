19fbd244a06a703219cf90245c50a9d35e9e1f6f
Debalin Das <debalin90@gmail.com>
2017-01-04 11:41:00 +0530

    Making the deployable a ZIP instead of a folder.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # This script simply takes the JAR produced as a result
-# of the Travis build and pushes it to the release branch.
+# of the Travis build, compresses and pushes it to the release branch.
 # Taken from http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/.
 
 rev=$(git rev-parse --short HEAD)
 
 cp -r scripts out/artifacts/multiplayer_game_engine_jar/
-cd out/artifacts
+cd out/artifacts/multiplayer_game_engine_jar
+zip multiplayer_game_engine.zip scripts multiplayer_game_engine.jar
+rm -rf scripts
+rm -rf multiplayer_game_engine.jar
 
 git init
 git config user.name "Debalin Das"
